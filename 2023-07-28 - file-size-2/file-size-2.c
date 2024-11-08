764faa0d5e711248ba563860b9a0f9a869fbf0dc
Saendhor <comix143.mariorossi@gmail.com>
2024-11-08 22:40:56 +0100

    feat: started implementing shared_data structure

--- 2023-07-28 - file-size-2/file-size-2.c
+++ 2023-07-28 - file-size-2/file-size-2.c
@@ -1,21 +1,59 @@
-#include <pthread.h>
 #include "my_bst.h"
+#include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+#define PATHSIZE 64
+
+typedef struct shared_data {
+    //Shared data structure
+    node_t* number_set;
+
+    //Semaphore(s)
+    //sem_t full_sem, empty_sem;
+
+    //Mutex(es)
+    pthread_mutex_t shared_data_mutex;
+
+} shared_data_t;
+
+typedef struct thread_data {
+    unsigned short id;
+    char path[PATHSIZE];
+
+} thread_data_t;
+
 
 int main (int argc, char* argv[]) {
-    node_t* root = new_node(15);
-    insert_key(&root, 66);
-    
+    shared_data_t* shared_data = (shared_data_t*) malloc (sizeof(shared_data_t));
+    shared_data->number_set = (node_t*) malloc(sizeof(node_t));
+
+    //Initialization
+    shared_data->number_set = NULL;
+/*    
+    if (sem_init(&shared_data->full_sem, 0, 0) != 0) {
+        perror("Error while initializating full_sem");
+        exit(EXIT_FAILURE);
+    }
+
+    if (sem_init(&shared_data->empty_sem, 0, N) != 0) {
+        perror("Error while initializating empty_sem");
+        exit(EXIT_FAILURE);
+    }
+*/
+    if (pthread_mutex_init(&shared_data->shared_data_mutex, NULL)) {
+        perror("Error while trying to initialize shared_data mutex");
+        exit(EXIT_FAILURE);
+    }
+
     if (argc < 2) {
         perror("Error with inputed parameters");
         exit(EXIT_FAILURE);
     }
 
-    print_tree_rec(root, 0);
 
     
     return 0;
--- 2023-07-28 - file-size-2/run.sh
+++ 2023-07-28 - file-size-2/run.sh
@@ -3,5 +3,5 @@ printf "\t gcc -l pthread -o file-size-2 file-size.c -g \n"
 gcc -l pthread -o file-size-2 file-size-2.c -g
 
 printf "[RUN] Executing with parameters\n"
-printf "\t file-size-2 dir-1 dir-2\n"
-./file-size-2 dir-1 dir-2
+printf "\t file-size-2 dir-1 dir-2 dir-3\n"
+./file-size-2 dir-1 dir-2 dir-3
