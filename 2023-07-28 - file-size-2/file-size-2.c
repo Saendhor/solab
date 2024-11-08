29b9fa5455f900e7f54d8828b9945a1f28e4e27d
Saendhor <comix143.mariorossi@gmail.com>
2024-11-08 20:03:44 +0100

    feat: implemented BST with relative functions

--- 2023-07-28 - file-size-2/file-size-2.c
+++ 2023-07-28 - file-size-2/file-size-2.c
@@ -1,55 +1,125 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 typedef struct node {
-    unsigned int key;
-    struct node* prev_pointer;
-    struct node* next_pointer;
+    int key;
+    struct node* left;
+    struct node* right;
 
 } node_t;
 
-typedef struct list {
-    node_t* head;
+//Create a new BST node
+node_t* new_node (int key) {
+    node_t* temp = (node_t*) malloc(sizeof(node_t));
+    
+    if (temp != NULL) {
+        temp->key = key;
+        temp->left = temp->right = NULL;
 
-} list_t;
-
-typedef struct shared_data {
-    list_t number_set;
-
-} shared_data_t;
-
-void initialize_list (list_t list, unsigned int key) {
-    list.head = (node_t*) malloc (sizeof(node_t));
-    //Initializing default values for head
-    list.head->key = key;
-    list.head->prev_pointer = NULL;
-    list.head->next_pointer = NULL;
+    }
+    return temp;
 }
 
-void list_insert_head (list_t* list, unsigned int key) {
-    node_t* new_node = (node_t*) malloc (sizeof(node_t));
-    //new_node initialization
-    new_node->key = key;
-    new_node->next_pointer = list->head;
-    new_node->prev_pointer = NULL;
-    //old head is new "next node"
-    list->head->prev_pointer = new_node;
-    list->head->next_pointer = NULL;
-    //Setting new head
-    list->head = new_node;
+int insert_key (node_t** rootptr, int key) {
+    node_t* root = *rootptr;
 
-}
-
-
-int main (int argc, char* argv[]) {
-
-    if (argc < 2) {
-        perror("Error with passed parameters");
-        exit(EXIT_FAILURE);
+    if (root == NULL){
+        //Tree empty
+        (*rootptr) = new_node(key);
+        return 1;
     }
 
+    if (key == root->key) {
+        //do nothing
+        return 0;
+    }
+
+    if (key < root->key) {
+        //Check left
+        return insert_key(&(root->left), key);
+    } else {
+        //Check right
+        return insert_key(&(root->right), key);
+    }
+}
+
+int find_key (node_t* root, int key) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    if (root->key == key){
+        return 1;
+    }
+
+    if (key < root->key){
+        return find_key(root->left, key);
+
+    } else {
+        return find_key(root->right, key);
+
+    }
+}
+
+// FUNCTIONS TO PRINT TREE //
+
+void print_tabs (int num_tabs) {
+    for (int i = 0; i < num_tabs; i++) {
+        printf ("\t");
+    }
+}
+
+void print_tree_rec (node_t* root, int lvl) {
+    if (root == NULL) {
+        print_tabs(lvl);
+        printf("---<empty>--\n");
+        return;
+    }
+
+    print_tabs(lvl);
+    printf("key = %d\n", root->key);
+    print_tabs(lvl);
+    printf("left\n");
+
+    print_tree_rec(root->left, lvl+1);
+    print_tabs(lvl);
+    printf("right\n");
+
+    print_tree_rec(root->right, lvl+1);
+
+    print_tabs(lvl);
+    printf("done!\n");
+
+}
+
+void print_tree (node_t* root) {
+    print_tree_rec(root, 0);
+}
+
+/////////////////////////////
+
+int main (int argc, char* argv[]) {
+    node_t* root = NULL;
+
+    insert_key(&root, 15);
+    insert_key(&root, 11);
+    insert_key(&root, 24);
+    insert_key(&root, 5);
+    insert_key(&root, 19);
+    insert_key(&root, 16);
+
+    print_tree(root);
+
+    printf("%d (%d)\n", 16 , find_key(root, 16));
+    printf("%d (%d)\n", 15 , find_key(root, 15));
+    printf("%d (%d)\n", 5 , find_key(root, 5));
+    printf("%d (%d)\n", 115 , find_key(root, 115));
+    printf("%d (%d)\n", 1 , find_key(root, 1));
+    printf("%d (%d)\n", 7 , find_key(root, 7));
 
     return 0;
 }
--- 2023-07-28 - file-size-2/run.sh
+++ 2023-07-28 - file-size-2/run.sh
@@ -1,6 +1,6 @@
 printf "[RUN] Compiling with parameters\n"
 printf "\t gcc -l pthread -o file-size-2 file-size.c -g \n"
-gcc -l pthread -o file-size-2 file-size.c -g
+gcc -l pthread -o file-size-2 file-size-2.c -g
 
 printf "[RUN] Executing with parameters\n"
 printf "\t file-size-2 dir-1 dir-2\n"
