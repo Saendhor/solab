34516d4c29494214c91f36bfdac260c8605a5e4f
Saendhor <comix143.mariorossi@gmail.com>
2024-11-08 22:12:11 +0100

    feat: separated bst into its own header file

--- 2023-07-28 - file-size-2/file-size-2.c
+++ 2023-07-28 - file-size-2/file-size-2.c
@@ -1,125 +1,22 @@
 #include <pthread.h>
+#include "my_bst.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <unistd.h>
 
-typedef struct node {
-    int key;
-    struct node* left;
-    struct node* right;
-
-} node_t;
-
-//Create a new BST node
-node_t* new_node (int key) {
-    node_t* temp = (node_t*) malloc(sizeof(node_t));
-    
-    if (temp != NULL) {
-        temp->key = key;
-        temp->left = temp->right = NULL;
-
-    }
-    return temp;
-}
-
-int insert_key (node_t** rootptr, int key) {
-    node_t* root = *rootptr;
-
-    if (root == NULL){
-        //Tree empty
-        (*rootptr) = new_node(key);
-        return 1;
-    }
-
-    if (key == root->key) {
-        //do nothing
-        return 0;
-    }
-
-    if (key < root->key) {
-        //Check left
-        return insert_key(&(root->left), key);
-    } else {
-        //Check right
-        return insert_key(&(root->right), key);
-    }
-}
-
-int find_key (node_t* root, int key) {
-    if (root == NULL) {
-        return 0;
-    }
-
-    if (root->key == key){
-        return 1;
-    }
-
-    if (key < root->key){
-        return find_key(root->left, key);
-
-    } else {
-        return find_key(root->right, key);
-
-    }
-}
-
-// FUNCTIONS TO PRINT TREE //
-
-void print_tabs (int num_tabs) {
-    for (int i = 0; i < num_tabs; i++) {
-        printf ("\t");
-    }
-}
-
-void print_tree_rec (node_t* root, int lvl) {
-    if (root == NULL) {
-        print_tabs(lvl);
-        printf("---<empty>--\n");
-        return;
-    }
-
-    print_tabs(lvl);
-    printf("key = %d\n", root->key);
-    print_tabs(lvl);
-    printf("left\n");
-
-    print_tree_rec(root->left, lvl+1);
-    print_tabs(lvl);
-    printf("right\n");
-
-    print_tree_rec(root->right, lvl+1);
-
-    print_tabs(lvl);
-    printf("done!\n");
-
-}
-
-void print_tree (node_t* root) {
-    print_tree_rec(root, 0);
-}
-
-/////////////////////////////
 
 int main (int argc, char* argv[]) {
-    node_t* root = NULL;
+    node_t* root = new_node(15);
+    insert_key(&root, 66);
+    
+    if (argc < 2) {
+        perror("Error with inputed parameters");
+        exit(EXIT_FAILURE);
+    }
 
-    insert_key(&root, 15);
-    insert_key(&root, 11);
-    insert_key(&root, 24);
-    insert_key(&root, 5);
-    insert_key(&root, 19);
-    insert_key(&root, 16);
+    print_tree_rec(root, 0);
 
-    print_tree(root);
-
-    printf("%d (%d)\n", 16 , find_key(root, 16));
-    printf("%d (%d)\n", 15 , find_key(root, 15));
-    printf("%d (%d)\n", 5 , find_key(root, 5));
-    printf("%d (%d)\n", 115 , find_key(root, 115));
-    printf("%d (%d)\n", 1 , find_key(root, 1));
-    printf("%d (%d)\n", 7 , find_key(root, 7));
-
+    
     return 0;
 }
