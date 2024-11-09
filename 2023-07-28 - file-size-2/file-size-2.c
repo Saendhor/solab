f77d86a271cfb671140a64d9c829c6d9945c1661
Saendhor <comix143.mariorossi@gmail.com>
2024-11-09 15:32:35 +0100

    feat: implemented basics of threads ADD

--- 2023-07-28 - file-size-2/file-size-2.c
+++ 2023-07-28 - file-size-2/file-size-2.c
@@ -6,8 +6,9 @@
 #include <string.h>
 #include <unistd.h>
 
-#define PATHSIZE 64
 #define NAMESIZE 16
+#define NUM_ADD_THR 2
+#define PATHSIZE 64
 
 typedef struct shared_data {
     //Shared data structure
@@ -58,6 +59,20 @@ void* thread_add (void* args) {
         in presenza di almeno due numeri in number_set
         estrarre il minimo e il massimo attuale e di re-inserire la loro somma
     */
+    thread_data_t* dt = (thread_data_t*) args;
+    
+    //Conveniently defining name
+    char myname[NAMESIZE];
+    sprintf(myname, "ADD-%d", dt->id);
+
+
+    printf("[%s] Assigned directory: '%s'\n", myname, dt->path);
+
+
+
+    //Closing
+    printf("[%s] Job completed. Closing thread\n", myname);
+    return NULL;
 
 }
 
@@ -73,6 +88,8 @@ int main (int argc, char* argv[]) {
     //Shared Thread Data
     pthread_t* pthread_dir = (pthread_t*) malloc (sizeof(pthread_t) * num_dir);
     thread_data_t* thread_dir_data = (thread_data_t*) malloc (sizeof(thread_data_t) * num_dir);
+    pthread_t* pthread_add = (pthread_t*) malloc (sizeof(pthread_t) * NUM_ADD_THR);
+    thread_data_t* thread_add_data = (thread_data_t*) malloc (sizeof(thread_data_t) * NUM_ADD_THR);
 
     //Initialization
     //Shared Data
@@ -98,7 +115,7 @@ int main (int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // Create thread DIR(s)
+    //Create thread(s) DIR
     printf("[MAIN] Creating %d thread(s) DIR\n", num_dir);
     for (int i = 0; i < num_dir; i++) {
         //Filling thread_dir_data parameters
@@ -111,12 +128,38 @@ int main (int argc, char* argv[]) {
             exit(EXIT_FAILURE);
         }
     }
-    
+
+    //Create thread(s) ADD
+    printf("[MAIN] Creating %d thread(s) ADD\n", NUM_ADD_THR);
+    for (int j = 0; j < NUM_ADD_THR; j++) {
+        //Filling thread_add_data parameters
+        thread_add_data[j].id = j+1;
+        memset(thread_add_data[j].path, 0, PATHSIZE);
+        thread_add_data[j].shared_data = shared_data;
+        //Initializing thread itself
+        if (pthread_create(&pthread_add[j], NULL, &thread_add, &thread_add_data[j]) != 0) {
+            perror("Error while creating new ADD thread");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    //MAIN STUFF
+    sleep(1);
 
     //Closing thread DIR(s)
+    printf("[MAIN] Closing %d thread(s) DIR\n", num_dir);
     for (int i = 0; i < num_dir; i++) {
         if (pthread_join(pthread_dir[i], NULL) != 0) {
             perror("Error while performing join on thread(s) DIR");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    //Closing thread ADD(s)
+    printf("[MAIN] Closing %d thread(s) DIR\n", NUM_ADD_THR);
+    for (int j = 0; j < NUM_ADD_THR; j++) {
+        if (pthread_join(pthread_add[j], NULL) != 0) {
+            perror("Error while performing join on thread(s) ADD");
             exit(EXIT_FAILURE);
         }
     }
